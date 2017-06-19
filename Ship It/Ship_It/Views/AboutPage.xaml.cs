aa2238d1f2046e7ed933dfb5b9dad31537c6d836
Thomas Robitzsch <thomas@robitzsch.org>
2017-06-19 17:28:52 +0200

    Scaffolding

---
+++ Ship It/Ship_It/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace Ship_It.Views
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Ship It/Ship_It/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using Ship_It.ViewModels;
+
+using Xamarin.Forms;
+
+namespace Ship_It.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
